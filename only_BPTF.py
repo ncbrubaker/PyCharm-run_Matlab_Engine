9903a693c8968f4498b2fd878bf34f2fc8aefee9
ncbrubaker <ncbrubaker@ksu.edu>
2022-06-24 15:00:11 -0500

    Trying to create an Python call on only BPTF.

---
+++ only_BPTF.py
@@ -0,0 +1,61 @@
+import numpy as np
+import matlab.engine
+import scipy.io as sio
+
+# Change path to where export_BPTF is saved
+# import sys
+# positionOfPath = 1
+    # File location
+# r = "C:/Users/ncbrubaker/Documents/export_bptf"
+# sys.path.insert( positionOfPath, r)
+
+# Run .m file
+eng = matlab.engine.start_matlab()
+# eng.demo(nargout=0)
+
+# Set Variables in Python
+tte = sio.io.loadmat('TTe.mat');
+ttr = sio.io.loadmat('TTr.mat');
+D = 30;
+alpha = 2;
+wal = {};
+wal["Walpha"] = alpha;
+wal["nuAlpha"] = 1;
+
+pn = 50e-3;
+max_iter = 300;
+learn_rate = 1e-3;
+n_sample = 50;
+
+rates = {};
+rates["ridge"] = pn;
+rates["learn_rate"] = learn_rate;
+rates["range"] = [1, 5];
+rates["max_iter"] = max_iter;
+
+samples = {};
+samples["max_iter"] = n_sample;
+samples["n_sample"] = n_sample;
+samples["save_sample"] = False;
+samples["run_name"] = 'alpha2-1';
+
+# [U, V, dummy, r_pmf] = PMF_Grad(CTr, CTe, D, ...
+#    struct('ridge',pn,'learn_rate',learn_rate,'range',[1,5],'max_iter',max_iter));
+# fprintf('PMF: %.4f\n', r_pmf);
+
+pmf = eng.PMF_Grad(ttr, tte, D, rates, nargout=4)
+
+
+
+
+#[Us_bptf Vs_bptf Ts_bptf] = BPTF(TTr, TTe, D, struct('Walpha',alpha, 'nuAlpha',1), ...
+#    {U,V,ones(D,TTr.size(3))}, struct('max_iter',n_sample,'n_sample',n_sample,'save_sample',false,'run_name','alpha2-1'));
+
+bptf = eng.BPTF(ttr, tte, D, wal, samples, nargout=3)
+
+# a = eng.workspace['a'] # get the variable 'a' from the workspace
+# print(a)
+
+print("Hello!")
+
+eng.quit()
